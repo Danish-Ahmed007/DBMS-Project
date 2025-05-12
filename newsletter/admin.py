22483c7dc2d5997b2a62b56747d9e98d81236963
Danish-Ahmed007 <165469250+Danish-Ahmed007@users.noreply.github.com>
2025-05-12 14:44:28 +0500

    upload admin in newsletter

---
+++ newsletter/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import NewsletterSubscription
+
+@admin.register(NewsletterSubscription)
+class NewsletterSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at', 'is_active')
+    list_filter = ('is_active', 'subscribed_at')
+    search_fields = ('email',)
+    date_hierarchy = 'subscribed_at'
